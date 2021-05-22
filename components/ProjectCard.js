d8d4e7fc0c75ecf3b0ac05b498f572c79490fbeb
ilhambara <g2a.ilhammubarok@gmail.com>
2021-05-22 14:36:11 +0700

    Edit: populate the content and give it styles

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -1,15 +1,27 @@
+import Image from 'next/image';
 import Link from 'next/link';
+import styles from '../styles/Home.module.css';
 
 export default function ProjectCard({ project }) {
-  const { title, description, slug } = project.fields;
+  const { title, description, tags, slug, thumbnail } = project.fields;
+
+  const { width, height } = thumbnail.fields.file.details.image;
 
   return (
-    <div>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <Link href={'/projects/' + slug}>
-        <a>See the project</a>
-      </Link>
+    <div className={styles.card__elements}>
+      <div className={styles.image}>
+        <Image src={'https:' + thumbnail.fields.file.url} width={width} height={height} />
+      </div>
+
+      <div className={styles.description}>
+        <Link href={'/projects/' + slug}>
+          <a className={styles.link}>
+            <h2>{title}</h2>
+          </a>
+        </Link>
+        <p>{description}</p>
+        <span>{tags}</span>
+      </div>
     </div>
   );
 }
