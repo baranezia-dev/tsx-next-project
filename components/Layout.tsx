d9ca5437fb98ad81418ad3f22d44c9c74e250def
ilhambara <g2a.ilhammubarok@gmail.com>
2021-05-25 18:40:36 +0700

    Edit: styled layout and slug file

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,20 @@
-import Head from 'next/head';
 import layout from '../styles/Layout.module.css';
 
 interface LayoutProps {}
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className={layout.container}>
-      <main className={layout.main}>
-        <div>{children}</div>
+    <>
+      <div className={layout.container}>
+        <main className={layout.main}>
+          <div>{children}</div>
 
-        <footer className={layout.footer}>
-          <p>Copyright 2021 - This Project</p>
-        </footer>
-      </main>
-    </div>
+          <footer className={layout.footer}>
+            <p>Copyright 2021 - This Project</p>
+          </footer>
+        </main>
+      </div>
+    </>
   );
 };
 
