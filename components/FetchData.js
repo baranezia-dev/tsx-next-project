40376da5f828206933a5b05b15cc03b9ebb6afd6
ilhambara <g2a.ilhammubarok@gmail.com>
2021-05-28 02:26:47 +0700

    Edit: fallback pages in js component

--- components/FetchData.js
+++ components/FetchData.js
@@ -1,16 +1,12 @@
 import layout from '../styles/Layout.module.css';
 
-interface FetchDataProps {}
-
-const FetchData: React.FC<FetchDataProps> = () => {
+export default function FetchData() {
   return (
-    <>
+    <div>
       <div className={layout.fetch__data}></div>
       <div className={layout.fetch__data}></div>
       <div className={layout.fetch__data}></div>
       <div className={layout.fetch__data}></div>
-    </>
+    </div>
   );
-};
-
-export default FetchData;
+}
