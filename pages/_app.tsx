b293cbc7477ab52c9c7d154ee79e7cb58e497268
ilhambara <g2a.ilhammubarok@gmail.com>
2022-01-27 01:34:46 +0700

    update: packages and use chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import type { AppProps } from 'next/app';
-import Layout from '../components/Layout';
-import '../styles/globals.css';
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "../components/Layout";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,46 @@
-import Head from "next/head";
 import { createClient } from "contentful";
+import Head from "next/head";
 import Header from "../components/Header";
 import ProjectCard from "../components/ProjectCard";
 import Button from "../components/Button";
 import styles from "../styles/Home.module.css";
 
 export async function getStaticProps() {
-	const client = createClient({
-		space: process.env.CONTENTFUL_SPACE_ID,
-		accessToken: process.env.CONTENTFUL_DELIVERY_API,
-	});
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_DELIVERY_API,
+  });
 
-	const res = await client.getEntries({ content_type: "project" });
+  const res = await client.getEntries({ content_type: "project" });
 
-	// any objects added are passing as a props called 'projects'
-	return {
-		props: { projects: res.items },
-		revalidate: 1, // in seconds for page re-generation
-	};
+  // any objects added are passing as a props called 'projects'
+  return {
+    props: { projects: res.items },
+    revalidate: 1, // in seconds for page re-generation
+  };
 }
 
 export default function Projects({ projects }) {
-	return (
-		<>
-			<Head>
-				<title>Projects | Next Portfolio</title>
-				<meta
-					name="description"
-					content="Separate project section for Ilham Bara's portfolio"
-				/>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>Projects | Next Portfolio</title>
+        <meta
+          name="description"
+          content="Separate project section for Ilham Bara's portfolio"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<Header />
+      <Header />
 
-			<div className={styles.cards}>
-				{projects.map((project) => (
-					<ProjectCard key={project.sys.id} project={project} />
-				))}
-			</div>
+      <div className={styles.cards}>
+        {projects.map((project) => (
+          <ProjectCard key={project.sys.id} project={project} />
+        ))}
+      </div>
 
-			<Button />
-		</>
-	);
+      <Button />
+    </>
+  );
 }
